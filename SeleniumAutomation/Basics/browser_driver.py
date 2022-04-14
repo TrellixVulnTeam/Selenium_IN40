0c927c4560af0af87e6a8084599855a50c2decb2
Bright Onwumere <brighthamza9@yahoo.com>
2022-04-15 00:26:16 +0200

    Basics folder added and finding_elements.py file added.

--- SeleniumAutomation/Basics/browser_driver.py
+++ SeleniumAutomation/Basics/browser_driver.py
@@ -2,11 +2,21 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
+from ..rough import Browser
 
-#open chrome driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # go to the url
 driver.get("http://demostore.supersqa.com/")
-time.sleep(6)
+time.sleep(10)
+driver.close()
+
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+
+# go to the url
+driver.get("http://demostore.supersqa.com/")
+time.sleep(10)
+driver.close()
---
+++ SeleniumAutomation/Basics/finding_elements.py
@@ -0,0 +1,44 @@
+from typing import List
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.remote.webdriver import WebDriver
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def start_browser() -> WebDriver:
+    browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    browser.get("http://demostore.supersqa.com/")
+    time.sleep(4)
+    return browser
+
+
+def stop_browser() -> None:
+    time.sleep(4)
+    driver.close()
+
+
+# Finding elements by ID
+driver = start_browser()
+site_header_cart: WebElement = driver.find_element(By.ID, "site-header-cart")
+print(site_header_cart.text)
+site_header_cart.click()
+stop_browser()
+
+# Finding elements by CSS Selector
+driver = start_browser()
+site_header_cart: WebElement = driver.find_element(By.CSS_SELECTOR, "ul[id='site-header-cart']")
+print(site_header_cart.text)
+site_header_cart.click()
+stop_browser()
+
+# Finding elements by class name
+driver = start_browser()
+product: WebElement = driver.find_element(By.CLASS_NAME, "product")
+products: List = driver.find_elements(By.CLASS_NAME, "product")
+print(product)
+print(products)
+stop_browser()
